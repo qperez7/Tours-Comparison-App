ac1fbf2cd525f3615449a9f5a9900d1712a66fae
Quirianyailet Perez <qperez1@usf.edu>
2024-11-22 21:33:40 -0500

    Fixing Fetch P1

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,44 +1,47 @@
 import { useState, useEffect } from "react"; // Gives error when I add React, {} from "react"
 
-const fetchTours = async () => {
-  await fetch("https://www.course-api.com/react-tours-project");
-};
 function ToursByComparison() {
   const [tours, setTours] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetchTours()
-      .then((response) => response.json())
-      .then((data) => {
-        setTours(data);
-        setLoading(false);
-      })
-      .catch((error) => {
-        setLoading(false);
-        return error;
-      });
-  });
+    const fetchTours = async () => {
+      const response = await fetch(
+        "https://www.course-api.com/react-tours-project"
+      );
+      const tours = await response
+        .json()
+        .then(() => {
+          setTours(tours);
+          setLoading(false);
+        })
+        .catch((error) => {
+          setLoading(false);
+          return error;
+        });
+    };
+    fetchTours();
+  }, []); //Dependancy array
 
+  const ShowMore = () => {
+    (prev) => prev.info;
+  };
   if (loading) return <p>Loading all tours...</p>;
   return (
-    (
-      <div>
-        <h2>Comparing Tours </h2>
-        <ul>
-          {tours.map((tour) => (
-            <li key={tour.id}>
-              <img src={tour.image[0].url} alt={tour.name}></img>
-              {tour.name}
-              {tour.info} ${tour.price}
-            </li>
-          ))}
-        </ul>
-        <button onClick={() => tours.info}> Show More</button>
-        <button onClick={() => tours}> Not Interested</button>
-      </div> // Working on buttons
-    ),
-    [] //Dependancy array
+    <div>
+      <h2>Comparing Tours </h2>
+      <ul>
+        {tours.map((tour) => (
+          <li key={tour.id}>
+            <img src={tour.image[0].url} alt={tour.name}></img>
+            {tour.name}
+            {tour.info} ${tour.price}
+          </li>
+        ))}
+      </ul>
+      <button onClick={ShowMore}> Show More</button>
+      <button onClick={() => tours}> Not Interested</button>
+    </div> // Working on buttons
   );
 }
 
